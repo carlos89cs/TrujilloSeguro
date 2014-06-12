0bbb05ba06fa472b304b945e746c6e0e58b8767b
carlos89cs <carlos89.cs@outlook.com>
2014-06-12 02:15:55 -0500

    Inicio(Login Activity)

--- app/src/main/java/com/muni/trujilloseguro/app/InicioActivity.java
+++ app/src/main/java/com/muni/trujilloseguro/app/InicioActivity.java
@@ -11,6 +11,7 @@ public class InicioActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        getSupportActionBar().hide();
     }
 
 
